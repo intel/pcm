8840be4128d4bf6113c1a42bd12e7b1c06a8d2f6
Dementiev, Roman <roman.dementiev@intel.com>
2017-01-10 13:01:22 +0100

    add batch file to build all PCM binaries (MSVC)
    Change-Id: Ic12579f7bd5ad6c872af095d072341178e6b77ba

---
+++ build_solution.bat
@@ -0,0 +1,10 @@
+
+REM change path to your VCVARS.BAT
+CALL "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
+SET "PATH=C:\Program Files (x86)\MSBuild\14.0\Bin\amd64;%PATH%"
+
+   msbuild  pcm-all.sln /p:Configuration=Release  /t:Clean,Build
+
+exit
+
+
