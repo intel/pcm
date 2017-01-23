219f35df1c5d0a148e81026aa454c15dc46525e8
Roman Dementiev <roman.dementiev@intel.com>
2017-01-23 14:12:25 +0100

    use snprintf instead of sprintf
    Change-Id: I15e297b5e93e0cf0fc9025e124879ad462939959

--- utils.h
+++ utils.h
@@ -113,26 +113,26 @@ inline std::string unit_format(IntType n)
     char buffer[1024];
     if (n <= 9999ULL)
     {
-        sprintf(buffer, "%4d  ", int32(n));
+        snprintf(buffer, 1024, "%4d  ", int32(n));
         return buffer;
     }
     if (n <= 9999999ULL)
     {
-        sprintf(buffer, "%4d K", int32(n / 1000ULL));
+        snprintf(buffer, 1024, "%4d K", int32(n / 1000ULL));
         return buffer;
     }
     if (n <= 9999999999ULL)
     {
-        sprintf(buffer, "%4d M", int32(n / 1000000ULL));
+        snprintf(buffer, 1024, "%4d M", int32(n / 1000000ULL));
         return buffer;
     }
     if (n <= 9999999999999ULL)
     {
-        sprintf(buffer, "%4d G", int32(n / 1000000000ULL));
+        snprintf(buffer, 1024, "%4d G", int32(n / 1000000000ULL));
         return buffer;
     }
 
-    sprintf(buffer, "%4d T", int32(n / (1000000000ULL * 1000ULL)));
+    snprintf(buffer, 1024, "%4d T", int32(n / (1000000000ULL * 1000ULL)));
     return buffer;
 }
 
