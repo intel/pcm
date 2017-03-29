b519ef19f7268caa67a927db8c57798d7decded9
Dementiev, Roman <roman.dementiev@intel.com>
2017-03-29 10:47:50 +0200

    basic support for Denverton
    Change-Id: I3e7f83d395060f9d2f1437946a270eecb6ed65ac

--- cpucounters.cpp
+++ cpucounters.cpp
@@ -593,7 +593,8 @@ void PCM::initCStateSupportTables()
         case ATOM_AVOTON:
         case ATOM_BAYTRAIL:
         case ATOM_CHERRYTRAIL:
-	case ATOM_APOLLO_LAKE:
+	    case ATOM_APOLLO_LAKE:
+        case ATOM_DENVERTON:
             PCM_CSTATE_ARRAY(pkgCStateMsr, PCM_PARAM_PROTECT({0,    0,  0x3F8,      0,  0x3F9,  0,  0x3FA,  0,      0,  0,  0 }) );
         case NEHALEM_EP:
         case NEHALEM:
@@ -657,7 +658,8 @@ void PCM::initCStateSupportTables()
         case ATOM_BAYTRAIL:
         case ATOM_AVOTON:
         case ATOM_CHERRYTRAIL:
-	case ATOM_APOLLO_LAKE:
+	    case ATOM_APOLLO_LAKE:
+        case ATOM_DENVERTON:
         case SKL_UY:
         case SKL:
             PCM_CSTATE_ARRAY(coreCStateMsr, PCM_PARAM_PROTECT({0,	0,	0,	0x3FC,	0,	0,	0x3FD,	0x3FE,	0,	0,	0}) );
@@ -1207,7 +1209,8 @@ bool PCM::detectNominalFrequency()
                || cpu_model == HASWELL
                || cpu_model == BROADWELL
                || original_cpu_model == ATOM_AVOTON
-	       || original_cpu_model == ATOM_APOLLO_LAKE
+	           || original_cpu_model == ATOM_APOLLO_LAKE
+               || original_cpu_model == ATOM_DENVERTON
                || cpu_model == SKL
                || cpu_model == KNL
                || cpu_model == SKX
@@ -1565,7 +1568,8 @@ bool PCM::checkModel()
         || cpu_model == ATOM_BAYTRAIL
         || cpu_model == ATOM_AVOTON
         || cpu_model == ATOM_CHERRYTRAIL
-	|| cpu_model == ATOM_APOLLO_LAKE
+	    || cpu_model == ATOM_APOLLO_LAKE
+        || cpu_model == ATOM_DENVERTON
         ) {
         cpu_model = ATOM;
     }
