405678c3e5f61e1445f69f366c2ce6a973a41b1b
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2017-05-04 11:26:44 +0300

    Fix broken links in log messages and related filenames

--- client_bw.cpp
+++ client_bw.cpp
@@ -46,18 +46,18 @@ protected:
         switch (sys_info.wProcessorArchitecture)
         {
         case PROCESSOR_ARCHITECTURE_AMD64:
-            wcscat_s(driver_filename, MAX_PATH, L"\\winpmem_64.sys");
+            wcscat_s(driver_filename, MAX_PATH, L"\\winpmem_x64.sys");
             if (GetFileAttributes(driver_filename) == INVALID_FILE_ATTRIBUTES)
             {
-                std::cout << "ERROR: winpmem_64.sys not found in current directory. Download it from https://volatility.googlecode.com/svn-history/r2813/branches/scudette/tools/windows/winpmem/binaries/winpmem_64.sys ." << std::endl;
+                std::cout << "ERROR: winpmem_x64.sys not found in current directory. Download it from https://github.com/google/rekall/raw/master/tools/pmem/resources/winpmem/winpmem_x64.sys ." << std::endl;
                 std::cout << "ERROR: Memory bandwidth statistics will not be available." << std::endl;
             }
             break;
         case PROCESSOR_ARCHITECTURE_INTEL:
-            wcscat_s(driver_filename, MAX_PATH, L"\\winpmem_32.sys");
+            wcscat_s(driver_filename, MAX_PATH, L"\\winpmem_x86.sys");
             if (GetFileAttributes(driver_filename) == INVALID_FILE_ATTRIBUTES)
             {
-                std::cout << "ERROR: winpmem_32.sys not found in current directory. Download it from https://volatility.googlecode.com/svn-history/r2813/branches/scudette/tools/windows/winpmem/binaries/winpmem_32.sys ." << std::endl;
+                std::cout << "ERROR: winpmem_x86.sys not found in current directory. Download it from https://github.com/google/rekall/raw/master/tools/pmem/resources/winpmem/winpmem_x86.sys ." << std::endl;
                 std::cout << "ERROR: Memory bandwidth statistics will not be available." << std::endl;
             }
             break;
