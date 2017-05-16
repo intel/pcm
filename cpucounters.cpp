516b99798e7f8af082d0b334a1b7cd78ce8b27ea
Dementiev, Roman <roman.dementiev@intel.com>
2017-05-16 08:55:51 +0200

    change event names
    Change-Id: I26bf30609ef30b4361dc30af8852deb9ff216a4b

--- cpucounters.cpp
+++ cpucounters.cpp
@@ -4281,9 +4281,9 @@ void ServerPCICFGUncore::program()
     {
         // monitor TxL0_POWER_CYCLES
         event[0] = Q_P_PCI_PMON_CTL_EVENT(0x26);
-        // monitor RxL_FLITS.DATA on counter 1
+        // monitor RxL_FLITS.ALL_DATA on counter 1
         event[1] = Q_P_PCI_PMON_CTL_EVENT(0x03) + Q_P_PCI_PMON_CTL_UMASK(0xF);
-        // monitor TxL_FLITS.NON_DATA+DATA on counter 2
+        // monitor TxL_FLITS.NON_DATA+ALL_DATA on counter 2
         event[2] = Q_P_PCI_PMON_CTL_EVENT(0x02) + Q_P_PCI_PMON_CTL_UMASK((0x97|0x0F));
         // monitor UPI CLOCKTICKS
         event[3] = Q_P_PCI_PMON_CTL_EVENT(0x01);
