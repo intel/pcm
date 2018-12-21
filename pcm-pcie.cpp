2563e518030024eac2ade32f66a559734d97f976
Dementiev, Roman <roman.dementiev@intel.com>
2018-12-21 15:38:37 +0100

    update copyright years
    Change-Id: I1e1be1efd36b9e8727a666a53d4bf490ec578ebc

--- pcm-pcie.cpp
+++ pcm-pcie.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2009-2013, Intel Corporation
+   Copyright (c) 2009-2018, Intel Corporation
    All rights reserved.
 
    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
