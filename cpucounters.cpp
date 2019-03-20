281281d927bc3a0a706b082fbc0dea928aa58dbf
Dementiev, Roman <roman.dementiev@intel.com>
2019-03-20 10:17:45 +0100

    drop debug messages
    Change-Id: Ib3fe6629fdc3b536880cf16aec20982bf125db87

--- cpucounters.cpp
+++ cpucounters.cpp
@@ -2724,7 +2724,7 @@ std::string PCM::getCPUFamilyModelString()
 
 void PCM::enableForceRTMAbortMode()
 {
-    std::cout << "enableForceRTMAbortMode(): forceRTMAbortMode=" << forceRTMAbortMode << std::endl;
+    // std::cout << "enableForceRTMAbortMode(): forceRTMAbortMode=" << forceRTMAbortMode << std::endl;
     if (!forceRTMAbortMode)
     {
         if (isForceRTMAbortModeAvailable() && (core_gen_counter_num_max < 4))
@@ -2756,7 +2756,7 @@ bool PCM::isForceRTMAbortModeEnabled() const
 
 void PCM::disableForceRTMAbortMode()
 {
-    std::cout << "disableForceRTMAbortMode(): forceRTMAbortMode=" << forceRTMAbortMode << std::endl;
+    // std::cout << "disableForceRTMAbortMode(): forceRTMAbortMode=" << forceRTMAbortMode << std::endl;
     if (forceRTMAbortMode)
     {
         for (auto m : MSR)
