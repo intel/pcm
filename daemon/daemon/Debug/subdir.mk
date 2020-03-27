dd94837bae9e95b4c3ed55d39ec25d48095147f3
Bruggeman, Otto G <otto.g.bruggeman@intel.com>
2020-03-27 19:01:43 +0100

    Cleanup: Makefile, change compile option -std=c++0x to -std=c++11
    Change-Id: I23544fea0ab8cb00ec8677a8e7eec34fa5b5a2c5

--- daemon/daemon/Debug/subdir.mk
+++ daemon/daemon/Debug/subdir.mk
@@ -22,7 +22,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -Wno-unknown-pragmas -std=c++0x -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -Wno-unknown-pragmas -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
