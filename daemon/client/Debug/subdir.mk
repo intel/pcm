58d798eb465b14b801c587831ec7d36fcc76f6d9
Otto GJ Bruggeman <otto.g.bruggeman@intel.com>
2021-04-13 16:18:00 +0200

    Respect CXXFLAGS and CFLAGS

--- daemon/client/Debug/subdir.mk
+++ daemon/client/Debug/subdir.mk
@@ -20,7 +20,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: C++ Compiler'
-	$(CXX) -std=c++11 -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	$(CXX) ${CXXFLAGS} -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
