7be19823ffb974935d844dfb278bd33f38e3d215
Roman Dementiev <roman.dementiev@intel.com>
2021-10-20 14:56:14 +0200

    test with simdjson (#38)
    test with simdjson

--- cppcheck.sh
+++ cppcheck.sh
@@ -1,5 +1,5 @@
 
-cppcheck $1 --force --enable=warning --inline-suppr -iPCM-Service_Win -j $2 2> cppcheck.out
+cppcheck $1 --force --enable=warning --inline-suppr -iPCM-Service_Win -isimdjson -j $2 2> cppcheck.out
 
 if [ -s cppcheck.out ]
 then
