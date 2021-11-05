1c49dae9f25fa0cf1e7895af948e9d6d46a80ed2
Dementiev, Roman <roman.dementiev@intel.com>
2021-11-05 11:28:04 +0100

    build with MSVC 2017 in the build batch file
    Change-Id: Ib7a593e873a49a9439e5d83088dc784047b58bb6

--- build_solution.bat
+++ build_solution.bat
@@ -1,9 +1,9 @@
 
 REM change path to your VCVARS.BAT
-CALL "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
-SET "PATH=C:\Program Files (x86)\MSBuild\14.0\Bin\amd64;%PATH%"
-REM CALL "c:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
-REM SET "PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\amd64;%PATH%"
+REM CALL "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
+REM SET "PATH=C:\Program Files (x86)\MSBuild\14.0\Bin\amd64;%PATH%"
+CALL "c:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
+SET "PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\amd64;%PATH%"
 
    msbuild  pcm-all.sln /p:Configuration=Release;Platform=x64  /t:Clean,Build /m 
 
