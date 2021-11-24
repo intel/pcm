a6e01ddb97d0c7c7d012861f45a6d23eb2936a2d
Dementiev, Roman <roman.dementiev@intel.com>
2021-11-24 08:45:44 +0100

    print diagnostic messages about old compiler version
    Change-Id: I055655b19053d8b95deb83a11f05ec24f98fb0d7

--- simdjson_wrapper.h
+++ simdjson_wrapper.h
@@ -17,5 +17,9 @@
             #pragma message("parsing events from 01.org/perfmon won't be supported because simdjson library is not found in simdjson/singleheader/simdjson.h")
             #pragma message("run 'git clone https://github.com/simdjson/simdjson.git' to get simdjson library")
         #endif
+    #else
+            #pragma message("The compiler is too old, it does not support '__has_include' directive and other c++ features required for simdjson library. Parsing events from 01.org/perfmon won't be supported.")
     #endif
+#else
+    #pragma message("The compiler is too old (g++ 6 or below). Parsing events from 01.org/perfmon won't be supported.")
 #endif
