e3848538c2215f227b3af456b543319fa02c37c1
Pavithran Pandiyan <pavithran.pandiyan@intel.com>
2022-12-22 10:21:58 +0100

    fix prometheus script argument handling

--- scripts/grafana/start-prometheus.sh
+++ scripts/grafana/start-prometheus.sh
@@ -9,29 +9,30 @@ then
   exit 1
 fi
 
-# check if argument is file or ip-address:port, create the prometheus.yml accordingly
-if [ -f "$1" ]; then
-  echo "creating prometheus.yml for hosts in targets file";
-  head -n -1 "prometheus.yml.template" > prometheus.yml
-  while read -r line; do
-    echo "    - targets: ['$line']" >> "prometheus.yml"
-  done < "$1"
-
-else
-  echo "creating prometheus.yml for $1 ";
-  sed "s#PCMSENSORSERVER#$1#g" prometheus.yml.template > prometheus.yml
-fi
-
 mkdir -p grafana_volume/dashboards
 mkdir -p prometheus_volume
 
 chmod -R 777 *_volume
 
 mkdir -p provisioning/datasources
-cp automatic_prometheus.yml provisioning/datasources/automatic.yml 
+cp automatic_prometheus.yml provisioning/datasources/automatic.yml
+
+# check if argument is file, create the prometheus.yml accordingly
+if [ -f "$1" ]; then
+  echo "creating prometheus.yml for hosts in targets file";
+  head -n -1 "prometheus.yml.template" > prometheus.yml
+  while read -r line; do
+    echo "    - targets: ['$line']" >> "prometheus.yml"
+  done < "$1"
+  curl -o grafana_volume/dashboards/pcm-dashboard.json $(head -1 $1)/dashboard/prometheus
+
+else
+  echo "creating prometheus.yml for $1 ";
+  sed "s#PCMSENSORSERVER#$1#g" prometheus.yml.template > prometheus.yml
+  curl -o grafana_volume/dashboards/pcm-dashboard.json $1/dashboard/prometheus
+fi
 
 echo Downloading PCM dashboard
-curl -o grafana_volume/dashboards/pcm-dashboard.json (head -1 $1)/dashboard/prometheus
 
 echo Starting prometheus
 docker run --name prometheus -d -p 9090:9090 -v $PWD/prometheus.yml:/etc/prometheus/prometheus.yml -v $PWD/prometheus_volume:/prometheus prom/prometheus
@@ -39,5 +40,4 @@ echo Starting grafana
 docker run -d --link=prometheus --name=grafana -p 3000:3000 -v $PWD/grafana_volume:/var/lib/grafana -v $PWD/provisioning:/etc/grafana/provisioning grafana/grafana
 
 echo Start browser at http://localhost:3000/ and login with admin user, password admin
-
 
