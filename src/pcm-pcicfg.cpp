2d26544b3e0acd02e611e946e728f823a61b5993
Dementiev, Roman <roman.dementiev@intel.com>
2023-04-25 15:35:10 +0200

    pcm-pcicfg: option to specify intel device id
    Change-Id: I7f731a7958c12e913161ddbb0473955e8c4e15f7

--- src/pcm-pcicfg.cpp
+++ src/pcm-pcicfg.cpp
@@ -21,10 +21,11 @@ using namespace pcm;
 
 void print_usage(const char * progname)
 {
-    std::cout << "Usage " << progname << " [-w value] [-d] group bus device function offset\n\n";
+    std::cout << "Usage " << progname << " [-w value] [-d] [-i ID] [group bus device function] offset\n\n";
     std::cout << "  Reads/writes 32-bit PCICFG register \n";
     std::cout << "   -w value  : write the value before reading \n";
     std::cout << "   -d        : output all numbers in dec (default is hex)\n";
+    std::cout << "   -i ID     : specify Intel device ID instead of group bus device function\n";
     std::cout << "   --version : print application version\n";
     std::cout << "\n";
 }
@@ -49,12 +50,16 @@ int mainThrows(int argc, char * argv[])
     uint32 value = 0;
     bool write = false;
     bool dec = false;
+    uint32 deviceID = 0;
 
     int my_opt = -1;
-    while ((my_opt = getopt(argc, argv, "w:d")) != -1)
+    while ((my_opt = getopt(argc, argv, "i:w:d")) != -1)
     {
         switch (my_opt)
         {
+        case 'i':
+            deviceID = (uint32)read_number(optarg);
+            break;
         case 'w':
             write = true;
             value = (pcm::uint32)read_number(optarg);
@@ -68,17 +73,17 @@ int mainThrows(int argc, char * argv[])
         }
     }
 
-    if (optind + 4 >= argc)
+    if (optind + ((deviceID)?0:4) >= argc)
     {
         print_usage(argv[0]);
         return -1;
     }
 
-    int group = (int)read_number(argv[optind]);
-    int bus = (int)read_number(argv[optind + 1]);
-    int device = (int)read_number(argv[optind+2]);
-    int function = (int)read_number(argv[optind+3]);
-    int offset = (int)read_number(argv[optind+4]);
+    int group = -1;
+    int bus = -1;
+    int device = -1;
+    int function = -1;
+    int offset = -1;
 
     #ifdef _MSC_VER
     // Increase the priority a bit to improve context switching delays on Windows
@@ -94,22 +99,49 @@ int mainThrows(int argc, char * argv[])
         return -1;
     }
     #endif
-    try {
-        PciHandleType h(group, bus, device, function);
-        if (!dec) std::cout << std::hex << std::showbase;
-        if (write)
-        {
-            std::cout << " Writing " << value << " to " << group << ":" << bus << ":" << device << ":" << function << "@" << offset << "\n";
-            h.write32(offset, value);
-        }
-        value = 0;
-        h.read32(offset, &value);
-        std::cout << " Read value " << value << " from " << group << ":" << bus << ":" << device << ":" << function << "@" << offset << "\n\n";
-    }
-    catch (std::exception & e)
+
+    auto one = [&dec,&write](const uint32 & group, const uint32 & bus, const uint32 & device, const uint32 & function, const uint32 & offset, uint32 value)
     {
-        std::cerr << "Error accessing registers: " << e.what() << "\n";
-        std::cerr << "Please check if the program can access MSR/PCICFG drivers.\n";
+
+        try {
+            PciHandleType h(group, bus, device, function);
+            if (!dec) std::cout << std::hex << std::showbase;
+            if (write)
+            {
+                std::cout << " Writing " << value << " to " << group << ":" << bus << ":" << device << ":" << function << "@" << offset << "\n";
+                h.write32(offset, value);
+            }
+            value = 0;
+            h.read32(offset, &value);
+            std::cout << " Read value " << value << " from " << group << ":" << bus << ":" << device << ":" << function << "@" << offset << "\n\n";
+        }
+        catch (std::exception& e)
+        {
+            std::cerr << "Error accessing registers: " << e.what() << "\n";
+            std::cerr << "Please check if the program can access MSR/PCICFG drivers.\n";
+        }
+    };
+
+    if (deviceID)
+    {
+        offset = (int)read_number(argv[optind]);
+        forAllIntelDevices([&deviceID,&one,&offset, &value](const uint32 group, const uint32 bus, const uint32 device, const uint32 function, const uint32 device_id)
+            {
+                if (deviceID == device_id)
+                {
+                    one(group, bus, device, function, offset, value);
+                }
+            });
     }
+    else
+    {
+        group = (int)read_number(argv[optind]);
+        bus = (int)read_number(argv[optind + 1]);
+        device = (int)read_number(argv[optind + 2]);
+        function = (int)read_number(argv[optind + 3]);
+        offset = (int)read_number(argv[optind + 4]);
+        one(group, bus, device, function, offset, value);
+    }
+
     return 0;
 }
