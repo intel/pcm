16539eb2739f86af5495a91fef5734e7fcc976e0
Alexander Antonov <alexander.antonov@linux.intel.com>
2023-12-11 01:12:47 -0800

    Print information about pcm-iio utility in help

--- src/pcm-iio.cpp
+++ src/pcm-iio.cpp
@@ -1429,6 +1429,9 @@ void print_PCIeMapping(const std::vector<struct iio_stacks_on_socket>& iios, con
 
 void print_usage(const string& progname)
 {
+    std::cout << "\n Intel(r) Performance Counter Monitor " << PCM_VERSION << "\n";
+    std::cout << "\n This utility measures IIO information\n\n";
+
     cout << "\n Usage: \n " << progname << " --help | [interval] [options] \n";
     cout << "   <interval>                           => time interval in seconds (floating point number is accepted)\n";
     cout << "                                        to sample performance counters.\n";
@@ -1454,16 +1457,13 @@ PCM_MAIN_NOTHROW;
 
 int mainThrows(int argc, char * argv[])
 {
-    if(print_version(argc, argv))
+    if (print_version(argc, argv))
         exit(EXIT_SUCCESS);
 
     null_stream nullStream;
     check_and_set_silent(argc, argv, nullStream);
 
     set_signal_handlers();
-
-    std::cout << "\n Intel(r) Performance Counter Monitor " << PCM_VERSION << "\n";
-    std::cout << "\n This utility measures IIO information\n\n";
 
     string program = string(argv[0]);
 
