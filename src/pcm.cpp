e8a7a772a9e13e0542ea2b784b66940f858c4747
Roman Dementiev <roman.dementiev@intel.com>
2024-03-16 14:08:42 +0100

    pcm: make metric version 2 default

--- src/pcm.cpp
+++ src/pcm.cpp
@@ -93,8 +93,7 @@ void print_help(const string & prog_name)
         << "                                        to a file, in case filename is provided\n"
         << "                                        the format used is documented here: https://www.intel.com/content/www/us/en/developer/articles/technical/intel-pcm-column-names-decoder-ring.html\n";
     cout << "  -i[=number] | /i[=number]          => allow to determine number of iterations\n";
-    cout << "  -m=integer | /m=integer            => metrics version (default = 1). If version is 2\n"
-         << "                                        then a few alternative metrics are shown (UTIL=C0 residency for cores, CFREQ=core frequency in GHz).\n";
+    cout << "  -m=integer | /m=integer            => metrics version (default = 2)\n";
     print_enforce_flush_option_help();
     print_help_force_rtm_abort_mode(37);
     cout << " Examples:\n";
@@ -1292,7 +1291,7 @@ int mainThrows(int argc, char * argv[])
     bool reset_pmu = false;
     bool disable_JKT_workaround = false; // as per http://software.intel.com/en-us/articles/performance-impact-when-sampling-certain-llc-events-on-snb-ep-with-vtune
     bool enforceFlush = false;
-    int metricVersion = 1;
+    int metricVersion = 2;
 
     parsePID(argc, argv, pid);
 
@@ -1391,7 +1390,7 @@ int mainThrows(int argc, char * argv[])
             }
             if (metricVersion == 0)
             {
-                metricVersion = 1;
+                metricVersion = 2;
             }
             continue;
         }
