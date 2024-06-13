cbeadc05dcbb8580575e554292b467149814d68e
Dementiev, Roman <roman.dementiev@intel.com>
2024-06-13 15:27:54 +0200

    increase fuzzing coverage (II)
    Change-Id: I19d632dbf025bbeca4f19b0ff987d9618ce202a2

--- tests/fuzz.sh
+++ tests/fuzz.sh
@@ -1,8 +1,11 @@
 
 CC=`which clang` CXX=`which clang++` cmake ..  -DCMAKE_BUILD_TYPE=Debug -DFUZZ=1 && mkdir -p corpus &&
 make urltest-fuzz pcm-fuzz -j &&
-LLVM_PROFILE_FILE="urltest.profraw" bin/tests/urltest-fuzz -max_total_time=2000 corpus &&
-LLVM_PROFILE_FILE="pcm.profraw" bin/tests/pcm-fuzz -max_total_time=2000 corpus &&
-llvm-profdata merge -sparse urltest.profraw pcm.profraw -o all.profdata &&
+LLVM_PROFILE_FILE="urltest.profraw" bin/tests/urltest-fuzz -max_total_time=1000 corpus &&
+LLVM_PROFILE_FILE="pcm.profraw" bin/tests/pcm-fuzz -max_total_time=500 corpus &&
+LLVM_PROFILE_FILE="pcm.no_perf.profraw" PCM_NO_PERF=1 bin/tests/pcm-fuzz -max_total_time=500 corpus &&
+LLVM_PROFILE_FILE="pcm.uncore_perf.profraw" PCM_USE_UNCORE_PERF=1 bin/tests/pcm-fuzz -max_total_time=500 corpus &&
+LLVM_PROFILE_FILE="pcm.nmi_watchdog.profraw" PCM_KEEP_NMI_WATCHDOG=1 bin/tests/pcm-fuzz -max_total_time=100 corpus &&
+llvm-profdata merge -sparse urltest.profraw pcm.profraw pcm.no_perf.profraw pcm.uncore_perf.profraw pcm.nmi_watchdog.profraw -o all.profdata &&
 llvm-cov report --summary-only -object ./bin/tests/pcm-fuzz -object ./bin/tests/urltest-fuzz -instr-profile=all.profdata
 
