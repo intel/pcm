2d60379644a01e5f8fa3a8c932808bb87da46b34
Dementiev, Roman <roman.dementiev@intel.com>
2024-06-13 13:58:41 +0200

    increase fuzzing duration
    Change-Id: If94ecaf7dc79afc8c7d4a5b9bab596557099073d

--- tests/fuzz.sh
+++ tests/fuzz.sh
@@ -1,8 +1,8 @@
 
 CC=`which clang` CXX=`which clang++` cmake ..  -DCMAKE_BUILD_TYPE=Debug -DFUZZ=1 && mkdir -p corpus &&
 make urltest-fuzz pcm-fuzz -j &&
-LLVM_PROFILE_FILE="urltest.profraw" bin/tests/urltest-fuzz -max_total_time=30 corpus &&
-LLVM_PROFILE_FILE="pcm.profraw" bin/tests/pcm-fuzz -max_total_time=30 corpus &&
+LLVM_PROFILE_FILE="urltest.profraw" bin/tests/urltest-fuzz -max_total_time=2000 corpus &&
+LLVM_PROFILE_FILE="pcm.profraw" bin/tests/pcm-fuzz -max_total_time=2000 corpus &&
 llvm-profdata merge -sparse urltest.profraw pcm.profraw -o all.profdata &&
 llvm-cov report --summary-only -object ./bin/tests/pcm-fuzz -object ./bin/tests/urltest-fuzz -instr-profile=all.profdata
 
