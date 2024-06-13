15552b0249b73c8ec2711c914a2aa2717152a043
Roman Dementiev <roman.dementiev@intel.com>
2024-06-13 11:42:24 +0200

    refactor tests/fuzz.sh

--- tests/fuzz.sh
+++ tests/fuzz.sh
@@ -1,7 +1,7 @@
 
-CC=`which clang` CXX=`which clang++` cmake ..  -DCMAKE_BUILD_TYPE=Debug -DFUZZ=1 && mkdir corpus &&
+CC=`which clang` CXX=`which clang++` cmake ..  -DCMAKE_BUILD_TYPE=Debug -DFUZZ=1 && mkdir -p corpus &&
 make urltest-fuzz &&
-bin/tests/urltest-fuzz -max_total_time=30 corpus &&
-llvm-profdata merge -sparse default.profraw -o default.profdata &&
-llvm-cov report --summary-only  ./bin/tests/urltest-fuzz -instr-profile=default.profdata
+LLVM_PROFILE_FILE="urltest.profraw" bin/tests/urltest-fuzz -max_total_time=30 corpus &&
+llvm-profdata merge -sparse urltest.profraw -o all.profdata &&
+llvm-cov report --summary-only  ./bin/tests/urltest-fuzz -instr-profile=all.profdata
 
