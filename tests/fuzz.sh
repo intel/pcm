5ad7a93d1227c5434ea8b0e52fbddf1ba5a433ae
Roman Dementiev <roman.dementiev@intel.com>
2024-06-24 16:56:35 +0200

    replace files with human readable corpus

--- tests/fuzz.sh
+++ tests/fuzz.sh
@@ -29,7 +29,29 @@ printf "GET /dashboard/prometheus/default HTTP/1.1\r\nHost: localhost\r\nAccept:
 printf "GET /dashboard HTTP/1.1\r\nHost: localhost\r\nAccept: */*\r\n\r\n" > corpus/11 &&
 printf "GET /favicon.ico HTTP/1.1\r\nHost: localhost\r\nAccept: */*\r\n\r\n" > corpus/12 &&
 LLVM_PROFILE_FILE="pcm-sensor-server.profraw" bin/tests/pcm-sensor-server-fuzz -max_total_time=$((10 * $factor)) corpus > /dev/null &&
-rm -rf corpus/* && cp ../tests/urltest-fuzz.corpus/* corpus/ && LLVM_PROFILE_FILE="urltest.profraw" bin/tests/urltest-fuzz -max_total_time=$((10 * $factor)) corpus > /dev/null &&
+rm -rf corpus/* &&
+printf "http://otto:test@www.intel.com/~otto/file1.txt" > corpus/1 &&
+printf "file://localhost/c/mnt/cd/file2.txt" > corpus/2 &&
+printf "ftp://otto%40yahoo.com:abcd%3B1234@www.intel.com:30/xyz.php?a=1&t=3" > corpus/3 &&
+printf "gopher://otto@hostname1.intel.com:8080/file3.zyx" > corpus/4 &&
+printf "www.intel.com" > corpus/5 &&
+printf "http://www.blah.org/file.html#firstmark" > corpus/6 &&
+printf "http://www.blah.org/file.html#firstmark%21%23" > corpus/7 &&
+printf "localhost" > corpus/8 &&
+printf "https://www.intel.com" > corpus/9 &&
+printf "://google.com/" > corpus/10 &&
+printf "https://intc.com/request?" > corpus/11 &&
+printf "htt:ps//www.intel.com" > corpus/12 &&
+printf "http://www.intel.com:66666/" > corpus/13 &&
+printf "http:///" > corpus/14 &&
+printf "http://[1234::1234::1234/" > corpus/15 &&
+printf "http://@www.intel.com" > corpus/16 &&
+printf "http://otto@:www.intel.com" > corpus/17 &&
+printf "https://:@www.intel.com" > corpus/18 &&
+printf "https://user:@www.intel.com" > corpus/19 &&
+printf "http:www.intel.com/" > corpus/20 &&
+printf "http://ww\x00\x00\x00rstmark\x0a" > corpus/21 &&
+LLVM_PROFILE_FILE="urltest.profraw" bin/tests/urltest-fuzz -max_total_time=$((10 * $factor)) corpus > /dev/null &&
 rm -rf corpus/* && LLVM_PROFILE_FILE="pcm.profraw" bin/tests/pcm-fuzz -max_total_time=$((5 * $factor)) corpus > /dev/null &&
 rm -rf corpus/* && LLVM_PROFILE_FILE="pcm.no_perf.profraw" PCM_NO_PERF=1 bin/tests/pcm-fuzz -max_total_time=$((5 * $factor)) corpus  > /dev/null &&
 rm -rf corpus/* && LLVM_PROFILE_FILE="pcm.uncore_perf.profraw" PCM_USE_UNCORE_PERF=1 bin/tests/pcm-fuzz -max_total_time=$((5 * $factor)) corpus > /dev/null &&
