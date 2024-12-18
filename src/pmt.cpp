731a08b6e895849c09d966ee618a6784895c1ab7
Dementiev, Roman <roman.dementiev@intel.com>
2024-12-18 13:45:52 +0100

    pcm-raw: add PMT XML lookup

--- src/pmt.cpp
+++ src/pmt.cpp
@@ -7,6 +7,11 @@
 #include <vector>
 #include <unordered_map>
 #include <iostream>
+#include <algorithm>
+
+#ifdef PCM_PUGIXML_AVAILABLE
+#include "pugixml/src/pugixml.hpp"
+#endif
 
 #ifdef __linux__
 #include <unistd.h>
@@ -79,6 +84,16 @@ public:
         }
         return t.at(uid).size();
     }
+    static std::vector<size_t> getUIDs()
+    {
+        auto t = getTelemetryFiles();
+        std::vector<size_t> result;
+        for (auto & guid : t)
+        {
+            result.push_back(guid.first);
+        }
+        return result;
+    }
     virtual ~TelemetryArrayLinux() override
     {
     }
@@ -124,6 +139,7 @@ class TelemetryArrayDummy : public TelemetryArrayInterface
 public:
     TelemetryArrayDummy(const size_t /* uid */, const size_t /* instance */) {};
     static size_t numInstances(const size_t /* uid */) { return 0; };
+    static std::vector<size_t> getUIDs() { return std::vector<size_t>(); };
     virtual ~TelemetryArrayDummy() override {};
     size_t size() override { return 0;}; // in bytes
     void load() override {};
@@ -150,6 +166,15 @@ size_t TelemetryArray::numInstances(const size_t uid)
 #endif
 }
 
+std::vector<size_t> TelemetryArray::getUIDs()
+{
+#ifdef __linux__
+    return TelemetryArrayLinux::getUIDs();
+#else
+    return TelemetryArrayDummy::getUIDs();
+#endif
+}
+
 TelemetryArray::~TelemetryArray() {}
 
 size_t TelemetryArray::size()
@@ -169,5 +194,118 @@ uint64 TelemetryArray::get(size_t qWordOffset, size_t lsb, size_t msb)
     assert(impl.get());
     return impl->get(qWordOffset, lsb, msb);
 }
+
+
+bool TelemetryDB::loadFromXML(const std::string& pmtXMLPath)
+{
+#ifdef PCM_PUGIXML_AVAILABLE
+    pugi::xml_document doc;
+    auto result = doc.load_file((pmtXMLPath + "/xml/pmt.xml").c_str());
+
+    if (!result)
+    {
+        std::cerr << "Error: failed to load " << pmtXMLPath << "/xml/pmt.xml" << std::endl;
+        return false;
+    }
+
+    constexpr bool debug = false;
+
+    auto guids = TelemetryArray::getUIDs();
+    for (pugi::xml_node mapping: doc.child("pmt").child("mappings").children("mapping"))
+    {
+        auto guid = read_number(mapping.attribute("guid").value());
+        if (std::find(guids.begin(), guids.end(), guid) == guids.end())
+        {
+            // std::cerr << " guid " << std::hex << guid << " not found in telemetry files" << std::endl;
+            continue;
+        }
+        if (debug) std::cout << "Found mapping with guid: " << mapping.attribute("guid").value() << std::endl;
+        const auto description = mapping.child("description").text().as_string();
+        if (debug) std::cout << "  Description: " << description << std::endl;
+        const auto xmlset = mapping.child("xmlset");
+        const auto basedir = xmlset.child("basedir").text().as_string();
+        const auto aggregator = xmlset.child("aggregator").text().as_string();
+        const auto aggregator_path = pmtXMLPath + "/xml/" + basedir + "/" + aggregator;
+        if (debug) std::cout << "  Aggregator XML path: " << aggregator_path << std::endl;
+
+        pugi::xml_document aggregatorDoc;
+        auto aggregatorResult = aggregatorDoc.load_file(aggregator_path.c_str());
+        if (!aggregatorResult)
+        {
+            std::cerr << "Error: failed to load " << aggregator_path << std::endl;
+            return false;
+        }
+
+        auto aggregatorNode = aggregatorDoc.child("TELEM:Aggregator");
+        const std::string aggregatorName = aggregatorNode.child("TELEM:name").text().as_string();
+        if (debug) std::cout << "  Agregator name: " << aggregatorName << std::endl;
+        PMTRecord record;
+        record.uid = guid;
+        for (pugi::xml_node sampleGroup: aggregatorNode.children("TELEM:SampleGroup"))
+        {
+            const auto sampleID = sampleGroup.attribute("sampleID").as_uint();
+            if (debug) std::cout << "    SampleID: " << sampleID << std::endl;
+            record.qWordOffset = sampleID;
+            for (pugi::xml_node sample: sampleGroup.children("TELC:sample"))
+            {
+                const auto name = sample.attribute("name").as_string();
+                const std::string sampleSubGroup = sample.child("TELC:sampleSubGroup").text().as_string();
+                record.fullName = aggregatorName + "." + sampleSubGroup + "." + name;
+                record.sampleType = sample.child("TELC:sampleType").text().as_string();
+                record.lsb = sample.child("TELC:lsb").text().as_uint();
+                record.msb = sample.child("TELC:msb").text().as_uint();
+                record.description = sample.child("TELC:description").text().as_string();
+                if (debug) std::cout << "      ";
+                if (debug) record.print(std::cout);
+                records.push_back(record);
+            }
+        }
+
+        if (debug) std::cout << std::endl;
+    }
+
+    return true;
+#else
+    (void)pmtXMLPath; // suppress warning
+    std::cerr << "Error: pugixml library is not available" << std::endl;
+    return false;
+#endif
+}
+
+std::vector<TelemetryDB::PMTRecord> TelemetryDB::lookup(const std::string & name)
+{
+    std::vector<PMTRecord> result;
+    for (auto & record : records)
+    {
+        if (record.fullName.find(name) != std::string::npos)
+        {
+            result.push_back(record);
+        }
+    }
+    return result;
+}
+
+std::vector<TelemetryDB::PMTRecord> TelemetryDB::ilookup(const std::string & name)
+{
+    std::vector<PMTRecord> result;
+    auto to_lower = [](const std::string & s) -> std::string
+    {
+        std::string result;
+        for (auto c : s)
+        {
+            result.push_back(std::tolower(c));
+        }
+        return result;
+    };
+    for (auto & record : records)
+    {
+        if (to_lower(record.fullName).find(to_lower(name)) != std::string::npos)
+        {
+            result.push_back(record);
+        }
+    }
+    return result;
+}
+
 
 }; // namespace pcm
